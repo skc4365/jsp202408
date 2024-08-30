1362227fded52b1f787d6658d7b5c6117e6da3e4
skc4365 <skc4365@gmail.com>
2024-08-30 12:07:40 +0900

    0830_servlet

--- ex0830/ProjServlet/src/main/java/servlet/LifeCycle.java
+++ ex0830/ProjServlet/src/main/java/servlet/LifeCycle.java
@@ -14,44 +14,45 @@ import jakarta.servlet.http.HttpServletResponse;
 public class LifeCycle extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	// 서블릿 생성 init()보다 먼저 실행하라고, 어너테이션을 달아줌.
     @PostConstruct
     public void myPostConstruct() {
-        System.out.println("myPostConstruct() 호출");
+        System.out.println("-----myPostConstruct() 호출");
     }
 
     @Override
     public void init() throws ServletException {
-        System.out.println("init() 호출");
+        System.out.println("-----init() 호출");
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        System.out.println("service() 호출");
+        System.out.println("-----service() 호출");
         super.service(req, resp);
     }        
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
             throws ServletException, IOException {
-        System.out.println("doGet() 호출");
+        System.out.println("-----doGet() 호출");
         req.getRequestDispatcher("/lifeCycle.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
             throws ServletException, IOException {
-        System.out.println("doPost() 호출");
+        System.out.println("-----doPost() 호출");
         req.getRequestDispatcher("/lifeCycle.jsp").forward(req, resp);
     }
 
     @Override
     public void destroy() {
-        System.out.println("destroy() 호출");
+        System.out.println("-----destroy() 호출");
     }
 
     @PreDestroy
     public void myPreDestroy() {
-        System.out.println("myPreDestroy() 호출");
+        System.out.println("-----myPreDestroy() 호출");
     }
 }
